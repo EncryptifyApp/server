88b2f8f4e72b404ab09d5c756cbdc4159582bee2
Riad Joul <riadjoul@gmail.com>
2024-08-21 01:14:52 +0100

    V1 RELEASED

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -2,63 +2,60 @@ require('dotenv').config()
 import fs from 'fs';
 import { User } from '../entities/User';
 import crypto from 'crypto';
-//@ts-ignore
-import { FileUpload } from "graphql-upload";
+
 
 class AuthService {
 
-  generateSessionToken(): string {
-    const token = crypto.randomBytes(32).toString("hex");
-    return token;
-  }
-
-  async AuthenticatedUser(
-    licenseKey: string,
-    username: string,
-    publicKey: string,
-    encryptedPrivateKey: string,
-    expoPushToken: string,
-    profilePic: FileUpload | null
-): Promise<string | null> {
-    try {
-        // Try to find the user
-        const user = await User.findOneOrFail({
-            where: {
-                licenseKey: licenseKey,
-            },
-        });
-
-        // Generate a new session token
-        const newSessionToken = this.generateSessionToken();
-
-        user.activeSessionToken = newSessionToken;
-        user.username = username;
-        user.publicKey = publicKey;
-        user.encryptedPrivateKey = encryptedPrivateKey;
-        user.expoPushToken = expoPushToken;
-
-        // Handle the profile picture if provided
-        if (profilePic) {
-            const { createReadStream, filename } = await profilePic;
-            const stream = createReadStream();
-            const path = `./uploads/${filename}`;
-            await new Promise((resolve, reject) =>
-                stream
-                    .pipe(fs.createWriteStream(path))
-                    .on("finish", resolve)
-                    .on("error", reject)
-            );
-            user.profileUrl = path;
-        }
-
-        await user.save();
-        // Return the new session token
-        return newSessionToken;
-    } catch (error) {
-        console.error(error);
-        return null;
+    generateSessionToken(): string {
+        const token = crypto.randomBytes(32).toString("hex");
+        return token;
     }
-}
+
+    async AuthenticateUser(
+        licenseKey: string,
+        username: string,
+        publicKey: string,
+        encryptedPrivateKey: string,
+        expoPushToken: string,
+    ): Promise<string | null> {
+        try {
+            // Try to find the user
+            const user = await User.findOneOrFail({
+                where: {
+                    licenseKey: licenseKey,
+                },
+            });
+
+            const userWithExpoToken = await User.findOneOrFail({
+                where: {
+                    expoPushToken: expoPushToken,
+                },
+            });
+
+            if (userWithExpoToken) {
+                userWithExpoToken.expoPushToken = undefined;
+                await userWithExpoToken.save();
+            }
+
+
+            user.activeSessionToken = undefined;
+            // Generate a new session token
+            const newSessionToken = this.generateSessionToken();
+
+            user.activeSessionToken = newSessionToken;
+            user.username = username;
+            user.publicKey = publicKey;
+            user.encryptedPrivateKey = encryptedPrivateKey;
+            user.expoPushToken = expoPushToken;
+
+            await user.save();
+            // Return the new session token
+            return newSessionToken;
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
 
 
 
